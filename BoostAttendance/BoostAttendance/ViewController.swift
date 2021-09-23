003ae9212113f67c286d3173d20c287c0bcd854f
tmfrlrkvlek <wltn102998@gmail.com>
2021-09-23 16:56:56 +0900

    feat: create project

---
+++ BoostAttendance/BoostAttendance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoostAttendance
+//
+//  Created by 이지수 on 2021/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
