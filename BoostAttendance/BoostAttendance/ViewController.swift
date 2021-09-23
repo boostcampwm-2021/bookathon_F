e6ab5269ea9e56776dcfd4f68d5a5e123291586a
dailynj <leena9901@gmail.com>
2021-09-23 23:35:02 +0900

    feat: lets go button 아이디 선택안되면 비활성화

--- BoostAttendance/BoostAttendance/ViewController.swift
+++ BoostAttendance/BoostAttendance/ViewController.swift
@@ -13,12 +13,16 @@ class ViewController: UIViewController {
 
     let camperIDList = ["S013_김태훈", "S036_이나정", "S045_이지수"]
     @IBOutlet weak var camperIDPickerView: UITextField!
+    @IBOutlet weak var letsGoButton: UIButton!
     
     
     var ref: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        letsGoButton.isEnabled = false
+        letsGoButton.backgroundColor = UIColor(rgb: 0x0055FB, alpha: 0.3)
+        
         // picker View
         camperIDPickerView.tintColor = .clear
         createPickerView()
@@ -50,6 +54,8 @@ extension ViewController: UITextFieldDelegate, UIPickerViewDelegate, UIPickerVie
     }
 
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        letsGoButton.isEnabled = true
+        letsGoButton.backgroundColor = UIColor(rgb: 0x0055FB, alpha: 1.0)
         camperIDPickerView.text = camperIDList[row]
         camperIDPickerView.font = UIFont.systemFont(ofSize: 17)
     }
@@ -58,7 +64,6 @@ extension ViewController: UITextFieldDelegate, UIPickerViewDelegate, UIPickerVie
         let pickerView = UIPickerView()
         pickerView.delegate = self
         camperIDPickerView.inputView = pickerView
-//        self.view.addSubview(camperIDPickerView)
     }
     
     func dismissPickerView() {
@@ -73,4 +78,23 @@ extension ViewController: UITextFieldDelegate, UIPickerViewDelegate, UIPickerVie
     @objc func action() {
         self.camperIDPickerView.endEditing(true)
     }
+}
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int, alpha: Float) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: CGFloat(alpha))
+   }
+
+    convenience init(rgb: Int, alpha: Float) {
+       self.init(
+           red: (rgb >> 16) & 0xFF,
+           green: (rgb >> 8) & 0xFF,
+           blue: rgb & 0xFF,
+           alpha: alpha
+       )
+   }
 }
