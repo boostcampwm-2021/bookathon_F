5abc0fab88e7463864063e21bf5ddacf72933a45
dailynj <leena9901@gmail.com>
2021-09-23 23:35:32 +0900

    feat: FSCalendar import

--- BoostAttendance/BoostAttendance/CheckInOutViewController.swift
+++ BoostAttendance/BoostAttendance/CheckInOutViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FSCalendar
 
 class CheckInOutViewController: UIViewController {
 
--- BoostAttendance/Pods/Target Support Files/Pods-BoostAttendance/Pods-BoostAttendance-frameworks.sh
+++ BoostAttendance/Pods/Target Support Files/Pods-BoostAttendance/Pods-BoostAttendance-frameworks.sh
@@ -175,6 +175,7 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/FSCalendar/FSCalendar.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FirebaseCore/FirebaseCore.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FirebaseCoreDiagnostics/FirebaseCoreDiagnostics.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FirebaseDatabase/FirebaseDatabase.framework"
@@ -186,6 +187,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/FSCalendar/FSCalendar.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FirebaseCore/FirebaseCore.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FirebaseCoreDiagnostics/FirebaseCoreDiagnostics.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FirebaseDatabase/FirebaseDatabase.framework"
