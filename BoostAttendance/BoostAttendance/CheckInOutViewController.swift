c4020f60cb8834074e15ba90982e8d27ea750764
dailynj <leena9901@gmail.com>
2021-09-24 04:24:00 +0900

    feat: checkInOut calendar 구현완료

--- BoostAttendance/BoostAttendance/CheckInOutViewController.swift
+++ BoostAttendance/BoostAttendance/CheckInOutViewController.swift
@@ -14,35 +14,26 @@ enum boostColor:Int {
 }
 
 class CheckInOutViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource {
-
+    
     @IBOutlet weak var calendarView: FSCalendar!
+    private var currentPage: Date?
+    private lazy var today: Date = {
+        return Date()
+        
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 선택날짜, 오늘 날짜 색 없애기
-        calendarView.appearance.eventDefaultColor = .clear
-        calendarView.appearance.eventSelectionColor = .clear
-        
-        // header
-//        calendarView.appearance.headerTitleAlignment = .left
-//        calendarView.layer.a
-        
-        
         calendarDateColorSetting()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
+        deselectDate()
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "yyyy-MM-dd"
         calendarView.delegate = self
         calendarView.dataSource = self
     }
     
-    func beforeAfterMonth(moveUp: Bool) {
-//        dateComponents.month = moveUp ? 1 : -1 self.currentPage = calendarCurrent.date(byAdding: dateComponents, to: self.currentPage ?? self.today) self.calendar.setCurrentPage(self.currentPage!, animated: true)
-    }
-    
     func calendarDateColorSetting() {
-        // 배경색
         calendarView.backgroundColor = UIColor(rgb: .main, alpha: 0.05)
-        calendarView.scrollEnabled = true // 스크롤
         calendarView.scrollDirection = .horizontal // 가로 스크롤
         calendarView.appearance.titleDefaultColor = UIColor(rgb: .back, alpha: 1.0) // 평일 날짜색
         calendarView.appearance.titleWeekendColor = UIColor(rgb: .back, alpha: 0.5) // 주말 날짜색
@@ -54,6 +45,30 @@ class CheckInOutViewController: UIViewController, FSCalendarDelegate, FSCalendar
         calendarView.appearance.headerTitleFont = UIFont.boldSystemFont(ofSize: 18)
         calendarView.appearance.weekdayTextColor = UIColor(rgb: .main, alpha: 1.0) // Sun, Mon...
         // 년월에 흐릿하게 보이는 애들 없애기
-        calendarView.appearance.headerMinimumDissolvedAlpha = 0
+        calendarView.appearance.headerMinimumDissolvedAlpha = 0.2
+    }
+    
+    func deselectDate() {
+        calendarView.today = nil
+        calendarView.allowsSelection = false
     }
 }
+
+//extension CheckInOutViewController {
+//    @IBAction func lastMonth(_ sender: Any) {
+//        self.moveCurrentPage(moveUp: false)
+//    }
+//
+//    @IBAction func nextMonth(_ sender: Any) {
+//        self.moveCurrentPage(moveUp: true)
+//    }
+//
+//    func moveCurrentPage(moveUp: Bool) {
+//        let cal = Calendar.current
+//        var dateComponents = DateComponents()
+//        dateComponents.month = moveUp ? 1 : -1
+//
+//        self.currentPage = cal.date(byAdding: dateComponents, to: self.currentPage ?? self.today)
+//        self.calendarView.setCurrentPage(self.currentPage!, animated: true)
+//    }
+//}
