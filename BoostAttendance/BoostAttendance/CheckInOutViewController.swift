5e91aea717a8759e705ded6fe0d581323ca4baab
dailynj <leena9901@gmail.com>
2021-09-23 23:17:59 +0900

    docs: add checkInOut ViewController

---
+++ BoostAttendance/BoostAttendance/CheckInOutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CheckInOutViewController.swift
+//  BoostAttendance
+//
+//  Created by 이나정 on 2021/09/23.
+//
+
+import UIKit
+
+class CheckInOutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
